f48fc15eb1b2b186e5ef2d1526c376d3e9675724
Luciano Machado <lucianoomachados@gmail.com>
2025-01-04 23:07:07 -0300

    feat: introduction to useLocation

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,4 +1,13 @@
+import { useLocation } from 'react-router-dom';
 import './style.css'
 export const About = () => {
-    return <h1>About</h1>
+    const {state} = useLocation();
+
+
+    return (
+        <div>
+            <h1>About</h1>
+            <p>{state}</p>
+        </div>
+    ) 
 }
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -5,7 +5,7 @@ export const Nav = () => {
         <nav className="nav">
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
+                <li><Link state={'This is the state from ABOUT'} to="/about">About</Link></li>
                 <li><Link to="/posts">Posts</Link></li>
                 <li><Link to="/posts/10">Post 10</Link></li>
                 <li><Link to="/redirect">Redirect</Link></li>
